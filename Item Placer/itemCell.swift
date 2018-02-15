5af7077a2d6a153d8c90c99ecedd43229d23be48
TrevKnows <Trevor.Beaton11@gmail.com>
2018-02-15 10:58:03 -0500

    Uploaded

---
+++ Item Placer/itemCell.swift
@@ -0,0 +1,19 @@
+//
+//  itemCell.swift
+//  Item Placer
+//
+//  Created by Trevor Beaton on 12/15/17.
+//  Copyright © 2017 Vanguard Logic LLC. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+  
+
+  @IBOutlet weak var itemLabel: UILabel!
+  
+
+
+
+}
